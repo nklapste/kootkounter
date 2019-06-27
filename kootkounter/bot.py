7a1f5fee0d026de8d2a1fde8f7fce5c1f6e03341
Nathan Klapstein <nklapste@ualberta.ca>
2019-06-27 16:04:16 -0400

    refactoring

--- kootkounter/bot.py
+++ kootkounter/bot.py
@@ -71,6 +71,42 @@ def degeneracy_detector(string: str) -> List[str]:
     return degeneracy
 
 
+WARN_COOLDOWN = 10
+
+
+class ThrottleException(Exception):
+    """Operation is being called too soon"""
+
+
+class ThrottleDecorator(object):
+    def __init__(self, func, interval):
+        self.func = func
+        self.interval = interval
+        self.last_run = 0
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self.func
+        return partial(self, obj)
+
+    def __call__(self, *args, **kwargs):
+        now = time.time()
+        if now - self.last_run < self.interval:
+            raise ThrottleException("Call after {0} seconds".format(
+                self.last_run + self.interval - now))
+        else:
+            self.last_run = now
+            return self.func(*args, **kwargs)
+
+
+def throttle(interval):
+    def applyDecorator(func):
+        decorator = ThrottleDecorator(func=func, interval=interval)
+        return wraps(func)(decorator)
+
+    return applyDecorator
+
+
 async def help_(message: Message):
     await message.channel.send("""```
 kootkounter: Tracking degeneracy across Discord!
@@ -83,10 +119,40 @@ Available commands:
 !KKregister DISCORD_USER_ID
     Register a Discord User for degeneracy tracking with the 
     specified DISCORD_USER_ID
-
+    
+!KKunregister DISCORD_USER_ID
+    Unregister a Discord User from degeneracy tracking with the 
+    specified DISCORD_USER_ID
+    
 !KKshow
     List collected degeneracy statistics on all registered Discord Users
 ```""")
+
+
+async def register(message: Message):
+    try:
+        degenerate_id = int(message.content.split()[1])
+        get_or_create(Degenerate, id=degenerate_id)
+        DB.commit()
+    except (IndexError, ValueError):
+        await message.channel.send(
+            "Registering user failed: Invalid arguments")
+    else:
+        await message.channel.send(
+            "Registered user: {}".format(degenerate_id))
+
+
+async def unregister(message: Message):
+    try:
+        degenerate_id = int(message.content.split()[1])
+        DB.delete(get_or_create(Degenerate, id=degenerate_id))
+        DB.commit()
+    except (IndexError, ValueError):
+        await message.channel.send(
+            "Unregistering user failed: Invalid arguments")
+    else:
+        await message.channel.send(
+            "Unregistered user: {}".format(degenerate_id))
 
 
 async def show(message: Message):
@@ -105,73 +171,21 @@ async def show(message: Message):
         )
 
 
-async def register(message: Message):
-    tokens = message.content.split()
-    if len(tokens) == 2:
-        try:
-            degenerate_id = int(tokens[1])
-            get_or_create(Degenerate, id=degenerate_id)
-            DB.commit()
-        except ValueError:
-            await message.channel.send(
-                "Registering user failed: Invalid arguments")
-        else:
-            await message.channel.send(
-                "Registered user: {}".format(degenerate_id))
-    else:
-        await message.channel.send(
-            "Registering user failed: Invalid arguments")
-
-
-class TooSoon(Exception):
-    """Can't be called so soon"""
-
-
-class CoolDownDecorator(object):
-    def __init__(self, func, interval):
-        self.func = func
-        self.interval = interval
-        self.last_run = 0
-
-    def __get__(self, obj, objtype=None):
-        if obj is None:
-            return self.func
-        return partial(self, obj)
-
-    def __call__(self, *args, **kwargs):
-        now = time.time()
-        if now - self.last_run < self.interval:
-            raise TooSoon("Call after {0} seconds".format(
-                self.last_run + self.interval - now))
-        else:
-            self.last_run = now
-            return self.func(*args, **kwargs)
-
-
-WARN_COOLDOWN = 10
-
-
-def CoolDown():
-    def applyDecorator(func):
-        decorator = CoolDownDecorator(func=func, interval=WARN_COOLDOWN)
-        return wraps(func)(decorator)
-
-    return applyDecorator
-
-
-@CoolDown()
+@throttle(WARN_COOLDOWN)
 async def warn_degenerate(message: Message):
     await message.channel.send("*{} will remember this*".format(BOT.user.name))
 
 
 @BOT.event
 async def on_message(message: Message):
-    if str(message.content).startswith("!KKregister"):
+    if str(message.content).startswith("!KKhelp"):
+        await help_(message)
+    elif str(message.content).startswith("!KKregister"):
         await register(message)
+    elif str(message.content).startswith("!KKunregister"):
+        await unregister(message)
     elif str(message.content).startswith("!KKshow"):
         await show(message)
-    elif str(message.content).startswith("!KKhelp"):
-        await help_(message)
     elif DB.query(Degenerate) \
             .filter(Degenerate.id == message.author.id).first():
         degeneracy = degeneracy_detector(message.content)
